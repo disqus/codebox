808456281c89c0e6466bf3889f1eff25e599782d
DZ <david@nodnod.net>
2011-03-20 14:27:06 -0700

    add ghetto fixtures

---
+++ codesharer/scripts/fixture.py
@@ -0,0 +1,26 @@
+import time
+import unittest2
+
+from codesharer.app import create_app
+from codesharer.apps.snippets.models import Snippet
+from flask import g, Response
+
+app = create_app()
+app.config.from_object('codesharer.conf.TestingConfig')
+client = app.test_client()
+_ctx = app.test_request_context()
+_ctx.push()
+app.preprocess_request()
+g.redis.flushdb()
+
+# Create sample snippets
+# plaintext
+Snippet.objects.create(org='disqus', author=1, text = "Hello World!")
+# python
+Snippet.objects.create(org='disqus', author=1, text = "print 'cramer sucks'")
+# html
+Snippet.objects.create(org='disqus', author=1, text = '<h1>Cramer sucks</h1>')
+# javascript
+Snippet.objects.create(org='disqus', author=1, text = "document.write('cramer sucks')")
+
+
