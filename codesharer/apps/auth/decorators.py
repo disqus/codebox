b0488190f8482d54b7031724e5ab4b441cc22556
David Cramer <dcramer@gmail.com>
2011-03-20 12:38:25 -0700

    Ensure login_required is compat with tests

--- codesharer/apps/auth/decorators.py
+++ codesharer/apps/auth/decorators.py
@@ -4,7 +4,7 @@ from functools import wraps
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None:
+        if getattr(g, 'user', None) is None:
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
