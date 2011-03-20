712753c1ecd5436cbc7da6af62004f0bd3423ab7
David Cramer <dcramer@gmail.com>
2011-03-20 11:42:42 -0700

    Added RedisHashMap

--- codesharer/utils/redis.py
+++ codesharer/utils/redis.py
@@ -37,28 +37,14 @@ class RedisOrderedDict(collections.MutableMapping):
         return self._r.zcard(self._name)
     
     def __getitem__(self, key):
-        score = self._r.zscore(self._name, encode_key(key))
         values = self._r.hgetall(self.get_key(key))
-        values['score'] = score
         return values
     
-    def __setitem__(self, key, values):
-        assert 'score' in values
-        score = values['score']
+    def __setitem__(self, key, score):
         self._r.zadd(self._name, encode_key(key), score)
-        
-        hkey = self.get_key(key)
-        for k, v in values.iteritems():
-            if v == 'score':
-                continue
-            self._r.hset(hkey, k, v)
     
     def __delitem__(self, key):
         self._r.zrem(self._name, encode_key(key))
-        self._r.delete(self.get_key(key))
-    
-    def get_key(self, key):
-        return '%s:%s' % (self._name, encode_key(key))
     
     def keys(self, start=0, end=-1):
         # we use zrevrange to get keys sorted by high value instead of by lowest
@@ -69,6 +55,47 @@ class RedisOrderedDict(collections.MutableMapping):
     
     def items(self, start=0, end=-1):
         return self._r.zrevrange(self._name, start, end, withscores=True)
+    
+    def get(self, key, default=0):
+        return self[key] or default
+    
+    def iteritems(self):
+        return iter(self)
+    
+    def clear(self):
+        self._r.delete(self._name)
+
+class RedisHashMap(collections.MutableMapping):
+    def __init__(self, r, name):
+        self._r = r
+        self._name = encode_key(name)
+    
+    def __iter__(self):
+        return iter(self.items())
+    
+    def __len__(self):
+        return self._r.hlen(self._name)
+    
+    def __contains__(self, key):
+        return self._r.hexists(self._name, encode_key(key))
+    
+    def __getitem__(self, key):
+        return self._r.hget(self._name, encode_key(key))
+    
+    def __setitem__(self, key, val):
+        self._r.hset(self._name, encode_key(key), val)
+    
+    def __delitem__(self, key):
+        self._r.hdel(self._name, encode_key(key))
+    
+    def keys(self):
+        return self._r.hkeys(self._name)
+    
+    def values(self):
+        return self._r.hvals(self._name)
+    
+    def items(self):
+        return self._r.hgetall(self._name).items()
     
     def get(self, key, default=0):
         return self[key] or default
