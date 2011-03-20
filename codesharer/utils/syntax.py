fcc36551609b94b351c5e471b7577f143b92b644
DZ <david@nodnod.net>
2011-03-20 14:10:17 -0700

    add pygments and colorize util

---
+++ codesharer/utils/syntax.py
@@ -0,0 +1,10 @@
+import pygments
+
+def colorize(code):
+    """
+    Takes in code and returns pygmentized code.
+    Attempts to guess insert.
+    """
+
+    # TODO: actually colorize
+    return code
