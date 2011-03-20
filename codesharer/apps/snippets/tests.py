1fa18e8153b2af4fe9e775a28b5eeaf6b08b76bd
David Cramer <dcramer@gmail.com>
2011-03-20 14:34:23 -0700

    Initial code for organizations

--- codesharer/apps/snippets/tests.py
+++ codesharer/apps/snippets/tests.py
@@ -32,9 +32,9 @@ class SnippetTestCase(FlaskTest):
         for i in xrange(3):
             time.sleep(0.01)
             res.append(Snippet.objects.create(
-                org='disqus',
+                org=1,
                 text='test %d' % i,
-                author=1,
+                user=1,
             ))
 
         self.assertEquals(Snippet.objects.count(), 3)
