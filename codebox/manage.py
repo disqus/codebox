783fe33a64e35ed34b77050ce3a9aaa78fcd561b
David Cramer <dcramer@gmail.com>
2011-07-25 17:52:11 -0700

    Ensure codebox is on path

--- codebox/manage.py
+++ codebox/manage.py
@@ -7,6 +7,10 @@ codebox.manage
 :copyright: (c) 2011 DISQUS.
 :license: Apache License 2.0, see LICENSE for more details.
 """
+import os, os.path
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
 
 from flaskext.actions import Manager
 
