dd8d4515f5e39dcc3f23db5a5acf3478c9c16ae2
David Cramer <dcramer@gmail.com>
2011-07-25 18:15:31 -0700

    Read janrain/secret key from env

--- codebox/conf.py
+++ codebox/conf.py
@@ -12,11 +12,9 @@ import urlparse
 class Config(object):
     DEBUG = True
     TESTING = False
-    SECRET_KEY = '\x89\x1d\xec\x8eJ\xda=C`\xf3<X\x81\xff\x1e\r{+\x1b\xe1\xd1@ku'
+    SECRET_KEY = os.environ.get('SECRET_KEY', '\x89\x1d\xec\x8eJ\xda=C`\xf3<X\x81\xff\x1e\r{+\x1b\xe1\xd1@ku')
     REDIS_DB = 0
-    JANRAIN_API_KEY = '288a1ca2fedb4e1d1780c320fa4082ae69640a52'
-    PODIO_CLIENT_ID = "dcramer@gmail.com"
-    PODIO_KEY = "f7qFIBcPTfTBLOd8ondkO9UGqU6uN1iG"
+    JANRAIN_API_KEY = os.environ.get('JANRAIN_API_KEY')
     DOMAIN_BLACKLIST = ['gmail.com', 'hotmail.com', 'live.com', 'msn.com', 'yahoo.com', 'googlemail.com', 'facebookmail.com']
 
 if os.environ.has_key('REDISTOGO_URL'):
