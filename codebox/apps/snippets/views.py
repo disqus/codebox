de9749b63a0ebd63d9d25e79e93fd3447824424a
David Cramer <dcramer@gmail.com>
2011-07-28 19:58:26 -0700

    Ensure we filter out deleted items

--- codebox/apps/snippets/views.py
+++ codebox/apps/snippets/views.py
@@ -21,6 +21,7 @@ def dashboard():
     for s in snippets:
         if not s.text:
             Snippet.objects.remove_from_index(s.pk, dashboard=g.user.pk)
+    snippets = filter(lambda x: x.text, snippets)
     snippets_users = User.objects.get_many([s.user for s in snippets])
     snippets_orgs = Organization.objects.get_many([s.org for s in snippets])
 
@@ -187,6 +188,7 @@ def list_snippets(org):
     for s in snippets:
         if not s.text:
             Snippet.objects.remove_from_index(s.pk, org=org.pk)
+    snippets = filter(lambda x: x.text, snippets)
     snippets_users = User.objects.get_many([s.user for s in snippets])
 
     return render_template('snippets/list.html', **{
